057cd490c47d7e43240fba1f67f3e268405dc2c8
ethanevertSSU <escalantee@sonoma.edu>
2025-02-26 17:01:25 -0800

    Ethan/password hashing (#5)
    * readme changes
    
    * added password hashing just to signup, need to compare when loggin in
    
    * hashing logic in place and tested, should be ready for merge
    
    * fixed linting errors
    
    * fixed bcrypt error (installed @types/bcrypt, that was the fix)
    
    ---------
    
    Co-authored-by: ethanescalante <ethan65g@gmail.com>

--- src/app/api/Login/route.ts
+++ src/app/api/Login/route.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import {NextResponse} from "next/server";
-
+import bcrypt from "bcrypt";
 
 
 const prisma = new PrismaClient();
@@ -18,7 +18,9 @@ export async function POST(req: Request) {
         });
 
 
-        let userPassword: string;
+        //let userPassword: string;
+
+
 
         if(!identifier || !password) {
             return NextResponse.json({ error: "Please Fill Out All Fields" }, {status: 400});
@@ -26,16 +28,17 @@ export async function POST(req: Request) {
 
         if(!user){
             return NextResponse.json({ error: "Invalid Username Or Password" }, {status: 400});
-        }else{
-            userPassword = user.password;
-            if(password !== userPassword){
-                return NextResponse.json({ error: "Invalid Username or Password" }, {status: 400});
-        }
-        }
+        }else {
 
-        console.log("Logged in Successfully!");
-        return NextResponse.json({ success: true, user: user }, { status: 201 });
+            const passwordCheck = bcrypt.compareSync(password.toString(), user.password);
 
+            if (!passwordCheck) {
+                return NextResponse.json({error: "Invalid Username or Password"}, {status: 400});
+
+            }
+            console.log("Logged in Successfully!");
+            return NextResponse.json({success: true, user: user}, {status: 201});
+        }
     }catch (error) {
         console.error("Login error:", error);
         return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
