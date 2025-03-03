f4ae456464f532f5263ecd7ed28be5ccf69df7e9
Ethan Escalante <ethan65g@gmail.com>
2025-03-02 21:55:59 -0800

    added authentication, need to add middleware and redirect for google

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,75 +1,100 @@
 "use client";
 
 import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 import Link from "next/link";
 import Header from "@/components/Header";
+import { authClient } from "@/app/lib/auth-client";
 
 export default function Login() {
-    const [identifier, setIdentifier] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const router = useRouter();
+  const [identifier, setIdentifier] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+  const handleGoogleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>,
+  ) => {
+    event.preventDefault();
+    setError("");
 
-        try {
-            const response = await fetch("/api/Login", {
-                method: "POST",
-                headers: {"content-type": "application/json"},
-                body: JSON.stringify({ identifier , password}),
-            });
-            const data = await response.json();
+    const result = await authClient.signIn.social({
+      provider: "google",
+    });
 
-            if (!response.ok) {
-                setError(data.error);
-                return;
-            }
-            setError("");
-            router.push("/dashboard");
+    if (result.error) {
+      setError(error || "Login failed. Please try again.");
+    } else {
+      console.log(result.data);
+      redirect("/dashboard");
+    }
+  };
 
-        } catch (error) {
-            console.error("Login Failed", error);
-            alert("Login Failed failed");
-        }
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setError("");
 
-    };
+    if (!identifier || !password) {
+      setError("Please enter credentials");
+    } else {
+      const result = await authClient.signIn.email({
+        email: identifier,
+        password: password,
+      });
 
-    return (
-        <div className="flex flex-col gap-3 h-screen items-center justify-center bg-purple-400">
-            <Header/>
-            <form
-                onSubmit={handleSubmit}
-                className="flex flex-col gap-3 h-screen items-center justify-center bg-purple-400"
-            >
-                <input
-                    className="border-2 rounded px-3 py-2 text-black"
-                    type="text"
-                    placeholder="Username Or Email"
-                    value={identifier}
-                    onChange={(e) => setIdentifier(e.target.value)}
-                />
+      if (result.error) {
+        setError(result.error.message || "Login failed. Please try again.");
+      } else {
+        console.log(result);
+        redirect("/dashboard");
+      }
+    }
+  };
 
-                <input
-                    className="border-2 rounded px-3 py-2 text-black"
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
+  return (
+    <span className="h-screen flex flex-col">
+      <Header />
+      <div className="flex h-screen flex-col gap-4 items-center justify-center bg-purple-400">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-3 items-center bg-purple-400"
+        >
+          <input
+            className="border-2 rounded px-3 py-2 text-black"
+            type="text"
+            placeholder="Email"
+            value={identifier}
+            onChange={(e) => setIdentifier(e.target.value)}
+          />
 
-                {error && <p className="text-red-600">{error}</p>}
+          <input
+            className="border-2 rounded px-3 py-2 text-black"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-                <button type="submit"
-                        className="border-2 rounded px-4 py-2 bg-purple-200 hover:bg-purple-600 text-black">
-                    Login
-                </button>
-                <span className="text-sm/2">Need to sign up? <Link
-        className="visited:text-purple-600 hover:underline hover:text-blue-600"
-        href="/Signup">Sign Up</Link></span>
+          {error && <p className="text-red-600">{error}</p>}
 
-            </form>
-        </div>
-            );
-            }
+          <button className="border-2 rounded px-4 py-2 bg-purple-200 hover:bg-purple-600 text-black">
+            Login
+          </button>
+        </form>
+        <form onSubmit={handleGoogleSubmit}>
+          <button className="border-2 rounded px-4 py-2 bg-purple-200 hover:bg-green-300 text-black">
+            Sign In With Google
+          </button>
+        </form>
+        <span className="text-sm">
+          Need to sign up?{" "}
+          <Link
+            className="visited:text-purple-600 hover:underline hover:text-blue-600"
+            href="/Signup"
+          >
+            Sign Up
+          </Link>
+        </span>
+      </div>
+    </span>
+  );
+}
--- src/app/api/Signup/route.ts
+++ src/app/api/Signup/route.ts
@@ -1,46 +1,11 @@
+"use server";
+
 import { PrismaClient } from "@prisma/client";
-import {NextResponse} from "next/server";
-import bcrypt from "bcrypt";
-
-
-const saltRounds = 10;
-
-const validEmail = (email: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
 
 const prisma = new PrismaClient();
-export async function POST(req: Request) {
-    try {
-        const { email, username, password, retype } = await req.json();
-        const checkUsername = await prisma.account.findFirst({ where: { username } });
-        const checkEmail = await prisma.account.findFirst({ where: { email } });
-        if (!username || !password || !email || !retype) {
-            return NextResponse.json({ error: "Please Fill Out All Fields" }, {status: 400});
-        }
-        if(!validEmail(email)) {
-            return NextResponse.json({ error: "Email Is Not Valid" }, {status: 400});
-        }
-        if(checkUsername) {
-           return NextResponse.json({ error: "Username Already Taken" }, {status: 400});
-        }if(checkEmail) {
-            return NextResponse.json({ error: "Email already taken" }, {status: 400});
-        }if(password !== retype) {
-            return NextResponse.json({ error: "Passwords do not match" }, {status: 400});
-        }
 
-        const hash: string = bcrypt.hashSync(password.toString(), saltRounds);
-
-        const newUser = await prisma.account.create({
-            data: { email ,username, password:hash },
-        });
-        console.log("created new user successfully");
-        return NextResponse.json({ success: true, user: newUser }, { status: 201 });
-
-    } catch (error) {
-        console.error("Signup error:", error);
-        return NextResponse.json({ error: "Internal Server Error, Try Again Later" }, { status: 500 });
-    }
+export async function checkUser(username: string) {
+  return prisma.user.findFirst({
+    where: { name: username },
+  });
 }
-
