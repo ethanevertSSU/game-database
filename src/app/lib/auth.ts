d6bd6a184e8779338907a20f199edcd5d81c3abb
ethanevertSSU <escalantee@sonoma.edu>
2025-03-04 21:46:21 -0800

    Ethan/auth setup (#8)
    * added authentication, need to add middleware and redirect for google
    
    * fixed form hydration issue, rest of the todos still apply
    
    * added middleware, auth, google callbacks, new env vars too
    
    * lint fixes
    
    * more lint fixing
    
    * fixed hydration issues on main page
    
    * CORS fix
    
    * dynamic url for betterAuth
    
    * fixed npm error
    
    * fixed CORS, and reverted SignUp changes
    
    ---------
    
    Co-authored-by: Ethan Escalante <ethan65g@gmail.com>

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,35 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@prisma/client";
+import { nextCookies } from "better-auth/next-js";
+import { oAuthProxy } from "better-auth/plugins";
+
+const prisma = new PrismaClient();
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      redirectURI: process.env.GOOGLE_REDIRECT_URI as string,
+    },
+  },
+  hooks: {
+    after: async (ctx) => {
+      console.log("Auth Hook Triggered:", ctx.path); // Debugging
+
+      if (ctx.path === "/sign-in/email") {
+        throw ctx.redirect("/dashboard");
+      }
+      return ctx;
+    },
+  },
+  redirectUrl: `/dashboard`,
+  plugins: [oAuthProxy(), nextCookies()],
+});
