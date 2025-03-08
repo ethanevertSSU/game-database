37aaaeb44474b766b1e93d335fcea3196372d323
Ethan Escalante <ethan65g@gmail.com>
2025-03-07 22:01:17 -0800

    removed unused variable

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -104,7 +104,7 @@ export default function LibraryPage() {
         onChange={handleSearch}
       />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
-        {filteredGames.map((game, i) => (
+        {filteredGames.map((game) => (
           <HoverCard key={game.id}>
             <HoverCardTrigger asChild>
               <button
