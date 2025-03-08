ce5f7d9f24b9039a32ecf0a360b47121fb2be8c0
ethanevertSSU <escalantee@sonoma.edu>
2025-03-07 20:20:54 -0800

    Bh/game form (#9)
    * Game form frontend
    
    * Made notes entry box larger
    
    * fixed some styling and changed link name to form
    
    ---------
    
    Co-authored-by: BHeggenhougen <heggenhoug@sonoma.edu>
    Co-authored-by: Ethan Escalante <ethan65g@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,31 @@
 import Link from "next/link";
 
 export default function Header() {
-    return (
-        <nav className="bg-purple-800 text-white drop-shadow-xl w-full flex items-center justify-between p-4">
-            <Link
-                href="/"
-                className="font-bold text-xl">
-                JEBBS Game Database
-            </Link>
-            <div className="flex space-x-4">
-                <Link
-                    href="/Signup"
-                    className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#6e33ff] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                >
-                    Sign Up
-                </Link>
-                <Link
-                    href="/Login"
-                    className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#6e33ff] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                >
-                    Log in
-                </Link>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="bg-purple-800 text-white drop-shadow-xl w-full flex items-center justify-between p-4">
+      <Link href="/" className="font-bold text-xl">
+        JEBBS Game Database
+      </Link>
+      <div className="flex space-x-4">
+        <Link
+          href="/form"
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#6e33ff] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+        >
+          Game Form Tester
+        </Link>
+        <Link
+          href="/Signup"
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#6e33ff] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+        >
+          Sign Up
+        </Link>
+        <Link
+          href="/Login"
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#6e33ff] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+        >
+          Log in
+        </Link>
+      </div>
+    </nav>
+  );
 }
