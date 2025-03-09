f7e6b1bca8a8b84c5dc0463ac21abc1b045959cf
ethanevertSSU <escalantee@sonoma.edu>
2025-03-08 20:12:35 -0800

    Added a profile with filler static data (#13)
    * Added a profile with filler static data
    
    * added default header
    
    * added profile to the header
    
    ---------
    
    Co-authored-by: Ed R <edr@Eds-MacBook-Pro.local>
    Co-authored-by: Ethan Escalante <ethan65g@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,6 +20,12 @@ export default function Header() {
           Game Form
         </Link>
         <Link
+          href="/profile"
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#6e33ff] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+        >
+          Profile
+        </Link>
+        <Link
           href="/Signup"
           className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#6e33ff] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
         >
