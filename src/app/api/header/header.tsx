dc887a873f1acde333a050a074e09751c10d9f9e
Ethan Escalante <152033559+ethanevertescalante@users.noreply.github.com>
2025-04-06 21:25:53 -0700

    Stuft/igdb and fixes (#30)
    * Updated basic Schema again and worked on profile backend fixes
    
    * Updated basic Schema again and worked on profile backend fixes
    
    * Updated searchableForm and igdb-search, still not working but in progress
    
    * Updated searchableForm and igdb-search, still not working but in progress
    
    * Updated searchableForm and igdb-search, still not working but in progress
    
    * Updated searchableForm and igdb-search, should be working now
    
    * Updated searchableForm and igdb-search, should have separate entries per platform, selectable physical or digital
    
    * Updated searchableForm to streamline game entry
    
    * Updated searchableForm to not silently fail when an image doesn't exist
    
    * Updated searchableForm to not crop images
    
    * Fixed Bugs i.e. background scaling, button centering
    
    ---------
    
    Co-authored-by: stuft <steven@tuft.org>

--- src/app/api/header/header.tsx
+++ src/app/api/header/header.tsx
@@ -11,6 +11,12 @@ export default async function HeaderIfAuthenticated() {
     return (
       <div className="flex space-x-4">
         <Link
+          href="/form"
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#6e33ff] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+        >
+          Form
+        </Link>
+        <Link
           href="/Signup"
           className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#6e33ff] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
         >
