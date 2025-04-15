b39b198f6f1e7e233cf1277982ec1945a547249f
Ethan Evert Escalante <152033559+ethanevertescalante@users.noreply.github.com>
2025-04-14 18:14:17 -0700

    Ethan/steam achievements (#32)
    * added steam link account, grabbing games and username
    
    * fully functioning steam link and disconnect
    
    * cleanup and such
    
    * linting fix
    
    * change tsx config
    
    * fixed prod returnurl
    
    * url change 2
    
    * added api call for getting last played game
    
    * added recentGame route
    
    * link is fine, unlink needs to show message to tell user to link to account
    
    * added loading state

---
+++ src/app/api/recentGame/route.ts
@@ -0,0 +1,57 @@
+import { auth } from "@/app/lib/auth";
+import { headers } from "next/headers";
+import { PrismaClient } from "@prisma/client";
+import { getLastedPlayedSteamGame } from "@/app/api/steam/steam";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+export async function GET() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  const username = session?.user.name ?? " ";
+
+  if (session) {
+    const user = await prisma.user.findFirst({
+      where: {
+        name: username,
+      },
+    });
+
+    if (user) {
+      const steamAccount = await prisma.linkedAccounts.findFirst({
+        where: {
+          userId: user.id,
+        },
+      });
+
+      if (steamAccount) {
+        const steamId = steamAccount?.externalPlatformId;
+        const lastGamePlayed = await getLastedPlayedSteamGame(steamId);
+        const lastGame = lastGamePlayed.response.games[0];
+
+        console.log("game: ", lastGame);
+
+        const gameName = lastGame?.name ?? " ";
+        const appId = lastGame?.appid ?? " ";
+        const fullPlaytime = lastGame?.playtime_forever ?? " ";
+
+        const hours = Math.floor(fullPlaytime / 60);
+        const minutes = fullPlaytime % 60;
+
+        return NextResponse.json(
+          {
+            gameName: gameName,
+            appId: appId,
+            hours: hours,
+            minutes: minutes,
+          },
+          { status: 201 },
+        );
+      }
+    }
+  }
+
+  return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+}
